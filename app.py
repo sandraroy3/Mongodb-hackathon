bc1646bd810a1e101ba11e9530ddfd33dd46176a
Sandra Roy <sandra.maria.roy.mec@gmail.com>
2021-10-26 13:00:37 -0400

    Added reister and login templates

--- app.py
+++ app.py
@@ -10,35 +10,42 @@ app.config['MONGO_URI'] = os.getenv('MONGO_URI2')
 
 mongo = PyMongo(app)
 
-todos = mongo.db.todos
+users = mongo.db.users
 
 @app.route('/')
 def index():
-    saved_todos = todos.find()
-    return render_template('index.html', todos=saved_todos)
+    return render_template('base.html')
 
-@app.route('/add', methods=['POST'])
-def add_todo():
-    new_todo = request.form.get('new-todo')
-    todos.insert_one({'text' : new_todo, 'complete' : False})
-    return redirect(url_for('index'))
+@app.route('/register')
+def register_user():
+    # saved_users = users.find()
+    return render_template('register.html')
+    # new_user = request.form.get('new-user')
+    # users.insert_one({'text' : new_user, 'complete' : False})
+    # return redirect(url_for('index'))
 
-@app.route('/complete/<oid>')
-def complete(oid):
-    todo_item = todos.find_one({'_id': ObjectId(oid)})
-    todo_item['complete'] = True
-    todos.save(todo_item)
-    return redirect(url_for('index'))
+@app.route('/login')
+def login_user():
+    # saved_users = users.find()
+    return render_template('login.html')
+  
 
-@app.route('/delete_completed')
-def delete_completed():
-    todos.delete_many({'complete' : True})
-    return redirect(url_for('index'))
+# @app.route('/users')
+# def complete(oid):
+#     user_item = users.find_one({'_id': ObjectId(oid)})
+#     user_item['complete'] = True
+#     users.save(user_item)
+#     return redirect(url_for('index'))
 
-@app.route('/delete_all')
-def delete_all():
-    todos.delete_many({})
-    return redirect(url_for('index'))
+# @app.route('/delete_completed')
+# def delete_completed():
+#     todos.delete_many({'complete' : True})
+#     return redirect(url_for('index'))
+
+# @app.route('/delete_all')
+# def delete_all():
+#     todos.delete_many({})
+#     return redirect(url_for('index'))
 
 # if __name__ == "__main__":
 #     app.run()
