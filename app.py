5a0a7da35d208cc1cf8ff78d52aaaacb7448324e
ettynan <ettynan@gmail.com>
2021-10-26 09:00:33 -0700

    adding app.py with some added user functions.

--- app.py
+++ app.py
@@ -10,35 +10,73 @@ app.config['MONGO_URI'] = os.getenv('MONGO_URI2')
 
 mongo = PyMongo(app)
 
-todos = mongo.db.todos
+users = mongo.db.users
+loans = mongo.db.loans
 
+# Operations for users
 @app.route('/')
 def index():
-    saved_todos = todos.find()
-    return render_template('index.html', todos=saved_todos)
+    return render_template('index.html', users=users.find())
 
-@app.route('/add', methods=['POST'])
-def add_todo():
-    new_todo = request.form.get('new-todo')
-    todos.insert_one({'text' : new_todo, 'complete' : False})
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    '''Registers/creates a new user'''
+    new_user = request.form.get('new-user')
+    users.insert_one({'text' : new_user})
     return redirect(url_for('index'))
 
-@app.route('/complete/<oid>')
-def complete(oid):
-    todo_item = todos.find_one({'_id': ObjectId(oid)})
-    todo_item['complete'] = True
-    todos.save(todo_item)
+@app.route('/find_user', methods=['GET'])
+def get_user_by_username(username):
+    ''' Returns the user_by username '''
+    query = {'username': username}
+    user = users.find_one(query)
+    return user if user else None
+
+@app.route('/login', methods=['POST'])
+def login(username, password):
+    ''' A function that takes in a username and password and returns user '''
+    query = {'username': username, 'password': password}
+    try:
+        user = users.find_one(query)
+        return user if user else None
+    except BaseException:
+        print('Unable to login')
+        return None
+
+
+#Operations for Loans
+
+
+# Operations for Applications
+@app.route('/add_application', methods=['POST'])
+def add_application_to_user():
+    '''Creates a new loan application for the user'''
+#TODO attach the application to a specific user. Query to find user based on username, then attach
+
+    new_application = request.form.get('new-application')
+    users.application.insert_one({'text' : users.application, 'complete' : False})
     return redirect(url_for('index'))
 
-@app.route('/delete_completed')
+@app.route('/complete/<oid>') #not sure we need this
+def complete(oid, username):
+    '''Takes in a user and an application id, updates it to mark complete'''
+    user = get_user_by_username(username)
+    application = user.applications.find_one({'_id': ObjectId(oid)})
+    application['complete'] = True
+    # users.applications.save(user.application)
+    return redirect(url_for('index'))
+
+@app.route('/delete_completed_application')
 def delete_completed():
-    todos.delete_many({'complete' : True})
+    '''Deletes all the applications marked complete'''
+    users.applications.delete_many({'complete' : True})
     return redirect(url_for('index'))
 
-@app.route('/delete_all')
-def delete_all():
-    todos.delete_many({})
-    return redirect(url_for('index'))
+# not sure it is needed
+# @app.route('/delete_all')
+# def delete_all():
+#     todos.delete_many({})
+#     return redirect(url_for('index'))
 
-# if __name__ == "__main__":
-#     app.run()
+if __name__ == "__main__":
+    app.run()
