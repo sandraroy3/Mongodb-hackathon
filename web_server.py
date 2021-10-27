ee7a2059f1670d340b2236b991303e3b7ae13658
Sandra Roy <sandra.maria.roy.mec@gmail.com>
2021-10-27 10:14:39 -0400

    redirection working now, popup to make

--- web_server.py
+++ web_server.py
@@ -4,6 +4,9 @@ from flask.globals import session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import bcrypt
+import time
+import tkinter as tk
+from tkinter import ttk
 
 app = Flask(__name__)
 
@@ -14,13 +17,17 @@ mongo = PyMongo(app)
 users = mongo.db.users
 loans = mongo.db.loans
 
-@app.route("/")
-def index():
+@app.route("/", methods=['POST', 'GET'])
+def base():
+    return render_template('base.html')
+
+@app.route("/logged")
+def logged():
     if 'username' in session:
-        return 'You are logged in as following user: ' + session['username']
+        return render_template('apply.html')
     return render_template("index.html")
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     login_user = users.find_one({"username" : request.form['username']})
     print(login_user)
@@ -28,7 +35,7 @@ def login():
         # if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
         if request.form['pass']==login_user['password']:
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            return redirect(url_for('logged'))
     return 'Invalid username or password combination'
 
 @app.route("/register", methods=['POST', 'GET'])
@@ -45,6 +52,46 @@ def register():
         return 'Username already exists'
     return render_template('register.html')
 
+@app.route('/update', methods=['POST'])
+def update_user():
+    if request.method =='POST':
+        users.update_one({'username': session['username']},
+                         {"$set": {'name': request.form['name'],
+                         'income': request.form['income'],
+                         'address': request.form['address']}})
+        update_status(request.form['income'])
+        # return redirect(url_for('base'))
+      
+    return render_template('apply.html')
+
+
+@app.route('/update_status', methods=['POST'])
+def update_status(income):
+    if request.method =='POST':
+        if int(income) > 50000:
+            users.update_one({'username': session['username']},
+                            {"$set": {'loan_status': 'Aproved'}})
+            popupmsg("You are approved:")
+        elif int(income) <= 50000:
+            users.update_one({'username': session['username']},
+                            {"$set": {'loan_status': 'DENIED'}})
+            popupmsg("You are Denied:")
+    return render_template('base.html')
+
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+# @app.route('/delete_completed_application')
+# def delete_completed():
+#     '''Deletes all the user applications marked complete'''
+#     users.applications.delete_many({'complete' : True})
+#     return redirect(url_for('index'))
 
 if (__name__ == '__main__'):
     app.secret_key='secretivekey'
